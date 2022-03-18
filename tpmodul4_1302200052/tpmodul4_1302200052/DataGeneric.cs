a5ad6509d745ed64d852e8872ae12ca2612fe0a4
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-03-18 11:06:57 +0700

    menambahkan class DataGeneric

---
+++ tpmodul4_1302200052/tpmodul4_1302200052/DataGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302200052
+{
+    internal class DataGeneric <T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("data yang tersimpan adalah: " + this.data);
+        }
+    }
+}
