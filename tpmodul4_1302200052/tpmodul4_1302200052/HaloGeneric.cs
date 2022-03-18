86082897c5d81274007caf8b5c2deed5a611ffdb
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-03-18 10:49:37 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul4_1302200052/tpmodul4_1302200052/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302200052
+{
+    internal class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("halo " + x);
+        }
+    }
+}
---
+++ tpmodul4_1302200052/tpmodul4_1302200052/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tpmodul4_1302200052
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            String x = "glen ";
+            halo.SapaUser(x);
+            
+        }
+    }
+
+
+}
